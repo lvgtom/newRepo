51e1eab4d1fd1c9a24c26cb5382c1aab2f71a188
Tom Horton <tom@visiongift.org>
2022-05-11 10:35:28 -0700

    Initial setup and chart building

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
-//Here we're importing items we'll need. You can add other imports here.
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+import c3 from "c3";
 
-//
+const months = ["Jan", "feb","Mar","Apr", "May","June"];
+
+window.loadChart = function (json) {
+  console.log(json);
+  const obj = JSON.parse(json);
+  console.log(obj);
+
+  const { type, data } = obj;
+
+  var chart = c3.generate({
+    size: {
+      width: 640
+    },
+    bindto: '#chart',
+    axis: {
+      x: {
+        type: "category",
+      },
+    },
+    data: {
+      onclick: function (clickedData) {
+        console.log(clickedData);
+        const {id, index} = clickedData;
+        const month = months[index];
+        console.log(id, index, month);
+        const object = { id, month };
+        console.log(object);
+        FileMaker.PerformScript("Get Data", JSON.stringify(object));
+      },
+      x:"x",
+      type: type,
+      colors: {
+        Mazda: '#ff0000',
+        Ford: '#00ff00',
+        Toyota: '#0000ff'
+    },
+      columns: data,
+    }
+  });
+}
